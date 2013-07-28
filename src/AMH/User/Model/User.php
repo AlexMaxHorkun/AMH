ddfb498c11e7a97e6c023dc9dc6077096456156b
AlexMaxHorkun <mindkilleralexs@mail.ru>
2013-07-28 13:46:57 +0300

    Started writing User module, User & Group models added

---
+++ src/AMH/User/Model/User.php
@@ -0,0 +1,51 @@
+<?php
+namespace AMH\User\Model;
+
+use \AMH\Model\Model as AMHModel;
+
+class User extends AMHModel{
+	protected function preConstruct(){
+		$this->addField(array(
+			'name'=>'id',
+			'set'=>AMHModel::FIELD_SET_ONLYONCE,
+			'type'=>'int',
+			'class'=>FALSE,
+			'typeAutocast'=>TRUE,
+		));
+		
+		$this->addField(array(
+			'name'=>'login',
+			'set'=>AMHModel::FIELD_SET_ONLYONCE,
+			'type'=>'string',
+			'class'=>TRUE,
+			'typeAutocast'=>TRUE,
+			'setCallback'=>function($val){
+				if(!$this->name){
+					$this->name=$val;
+				}
+				return $val;
+			},
+		));
+		
+		$this->addField(array(
+			'name'=>'name',
+			'set'=>AMHModel::FIELD_SET_ONLYONCE,
+			'type'=>'string',
+			'class'=>TRUE,
+			'typeAutocast'=>TRUE,
+		));
+		
+		$this->addField(array(
+			'name'=>'group',
+			'set'=>AMHModel::FIELD_SET_ONLYONCE,
+			'type'=>FALSE,
+			'class'=>'\AMH\User\Model\Group',
+			'typeAutocast'=>FALSE,
+		));
+	}
+	
+	public function __toString(){
+		return $this->name.':'.$this->group;	
+	}
+}
+?>
