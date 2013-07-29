62ec8f376b894ba0640d4bb7c733dd0268a52ec5
AlexMaxHorkun <mindkilleralexs@mail.ru>
2013-07-29 22:31:26 +0300

    now there is AMH key in config, different modules can be enabled/disabled

--- Module.php
+++ Module.php
@@ -2,6 +2,17 @@
 namespace AMH;
 
 class Module{
+	protected $config=array(
+		'enable'=>array('User'),
+	);
+	
+	public function onBootstrap($e){
+		$conf=$e->getApplication()->getConfig();
+		if(isset($conf['AMH'])){
+			$this->config=array_merge($this->config,$conf['AMH']);
+		}
+	}	
+	
 	function getAutoloaderConfig(){
 		return array(
 			'Zend\Loader\StandardAutoloader'=>array(
@@ -12,14 +23,29 @@ class Module{
 		);
 	}
 	
-	/*function getServiceConfig(){
-		return [
-			'factories'=>[
-				'modelTest'=>function($sm){
-					
-				}
-			],
-		];
-	}*/
+	function getServiceConfig(){
+		$serviceConfig=array('factories'=>array(
+			
+		));
+		if(in_array('User',$this->config['enable'])){
+			$serviceConfig['factories']=array_merge($serviceConfig['factories'],array(
+				'AMH\User\Mapper\User'=>function($sm){
+					return new \AMH\User\Mapper\User($sm->get('AMH\User\Pdo'),array('getArrayOnly'=>FALSE));
+				},
+				
+				'AMH\User\Pdo'=>function($sm){
+					$pdo=new \PDO('mysql:host=localhost;dbname=amh;','root','4837570Mind');
+					$pdo->setAttribute(\PDO::ATTR_ERRMODE,\PDO::ERRMODE_EXCEPTION);
+					return $pdo;
+				},
+				
+				'AMH\User\Mapper\Group'=>function($sm){
+					return new \AMH\User\Mapper\Group($sm->get('AMH\User\Pdo'),array('getArrayOnly'=>FALSE));
+				},
+			));
+		}
+		
+		return $serviceConfig;
+	}
 }
 ?>
